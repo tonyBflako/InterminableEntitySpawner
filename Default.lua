dea41ad81ab8b8297b06f05af8d9becf59bce165
tonyBflako <89006064+tonyBflako@users.noreply.github.com>
2023-03-07 23:29:15 -0600

    Create Default.lua

---
+++ Default.lua
@@ -0,0 +1,32 @@
+return {
+    Speed = 100,
+    DelayTime = 2,
+    HeightOffset = 0,
+    CanKill = true,
+    KillRange = 50,
+    BackwardsMovement = false,
+    Cycles = {
+        Min = 1,
+        Max = 1,
+        WaitTime = 2
+    },
+    CamShake = {
+        true,
+        {5, 15, 0.1, 1},
+        100
+    },
+    Jumpscare = {
+        false,
+        {}
+    },
+    FaceShake = {
+        true,
+        {-25, 25},
+        100
+    },
+    RotationShake = {
+        true,
+        {{0.75, 15}, {0.1, 1}, {0.1, 1}}
+        100
+    },
+}
